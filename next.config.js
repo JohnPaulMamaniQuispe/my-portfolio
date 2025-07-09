adae1461b760685a9538ebbd29e12247ce2b5334
johnmamani <john.jpmq@gmail.com>
2025-07-08 20:46:42 -0500

    Preparando para despliegue en Netlify

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    // Configuración básica para el portafolio
-    images: {
-      domains: ['localhost'], // Para imágenes locales
-    },
-  };
-  
-  module.exports = nextConfig;
